f857b708078a6c9e9152eee7408dc209328c0d3c
Ghozi-Waridi <ghoziwaridi@gmail.com>
2025-09-16 22:46:12 +0700

    temperatur

--- apichat/service.py
+++ apichat/service.py
@@ -40,7 +40,7 @@ def get_groq_response(session_id: int, user_prompt: str):
         chat_completion = client.chat.completions.create(
             messages=messages_for_groq,
             model="gemma2-9b-it",
-            # temperature=0.2,
+            temperature=0.2,
             # max_tokens=200,
         )
 
