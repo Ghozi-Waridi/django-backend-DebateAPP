084de0d48defb2e8c910d0306669f4d44af2529f
Ghozi-Waridi <ghoziwaridi@gmail.com>
2025-09-16 16:36:40 +0700

    :up: UPDATE: File Settings.

--- debat_LLM/settings.py
+++ debat_LLM/settings.py
@@ -89,8 +89,8 @@ WSGI_APPLICATION = "debat_LLM.wsgi.application"
 #     )
 # }
 DATABASES = {
-    "default": dj_database_url.config(
-        default=os.environ.get("DATABASE_URL"), conn_max_age=600
+    'default': dj_database_url.config(
+        default=os.getenv('SUPABASE_DB_URL')
     )
 }
 
